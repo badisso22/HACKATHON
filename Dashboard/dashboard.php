c4bf27de5f8277b260de19680646b884c8d49f7d
badisso <127608561+badisso22@users.noreply.github.com>
2025-05-02 17:26:59 +0100

    Update dashboard.php

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -148,7 +148,7 @@ $vocabularyWords = getRandomVocabularyWords($selected_language);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Mura</title>
-    <link rel="stylesheet" href="styles/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
